1c0ac4356693ce38ca71758d652096cb0768c5c5
Rob174 <robin.128.456@gmail.com>
2020-09-11 10:37:46 +0200

    Questions 1 et 2 de l'act1 faites

--- Act1/Src/main.c
+++ Act1/Src/main.c
@@ -42,7 +42,10 @@ int main(void)
 	
 	// Lancement chronomètre
 	Chrono_Start(); 
-  
+
+  SPI1->CR1 |= SPI_CR1_CPOL;
+  SPI1->CR1 &= ~(SPI_CR1_DFF);
+  USART1->CR2 |= ~(USART_CR2_STOP_0) & USART_CR2_STOP_1 & USART_CR2_STOP_Msk;
   /* Infinite loop */
   while (1)
   {
