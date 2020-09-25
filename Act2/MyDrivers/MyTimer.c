9a069ead7b9ba623e7e0bf00e710a21567b5ad7f
Rob174 <robin.128.456@gmail.com>
2020-09-25 16:15:01 +0200

    Ajout act2

---
+++ Act2/MyDrivers/MyTimer.c
@@ -0,0 +1,155 @@
+// TOUT A FAIRE !! //
+
+/*
+ indispensable pour pouvoir adresser les registres des périphériques.
+ Rem : OBLIGATION d'utiliser les définitions utiles contenues dans ce fichier (ex : TIM_CR1_CEN, RCC_APB1ENR_TIM2EN ...)
+ pour une meilleure lisibilité du code.
+ Pour les masques, utiliser également les définitions proposée
+ Rappel : pour mettre à 1  , reg = reg | Mask (ou Mask est le représente le ou les bits à positionner à 1)
+				  pour mettre à 0  , reg = reg&~ Mask (ou Mask est le représente le ou les bits à positionner à 0)
+ 
+*/ 
+#include "stm32f103xb.h" 
+
+void (*TIM1_handler_fct)(void);
+void (*TIM2_handler_fct)(void);
+void (*TIM3_handler_fct)(void);
+void (*TIM4_handler_fct)(void);
+
+void MyTimer_Start(TIM_TypeDef * Timer)
+{
+	/*
+	Timer = TIM1, TIM2, TIM3 ou TIM4:
+	
+	TIM1EN: TIM1 timer clock enable
+		Set and cleared by software.
+		0: TIM1 clock disabled
+		1: TIM1 clock enabled
+	*/
+	
+	// Identifier quel TIM est donné en paramètre et activer celui voulu
+	if(Timer == TIM1)
+		RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
+	else if(Timer == TIM2)
+		RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+	else if(Timer == TIM3)
+		RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
+	else if(Timer == TIM4)
+		RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
+}
+
+void MyTimer_Stop(TIM_TypeDef * Timer)
+{
+	/*
+	Timer = TIM1, TIM2, TIM3 ou TIM4:
+	
+	TIM1EN: TIM1 timer clock enable
+		Set and cleared by software.
+		0: TIM1 clock disabled
+		1: TIM1 clock enabled
+	*/
+	
+	// Identifier quel TIM est donné en paramètre et désactiver celui voulu
+	if(Timer == TIM1)
+		RCC->APB2ENR &= ~(RCC_APB2ENR_TIM1EN);
+	else if(Timer == TIM2)
+		RCC->APB1ENR &= ~(RCC_APB1ENR_TIM2EN);
+	else if(Timer == TIM3)
+		RCC->APB1ENR &= ~(RCC_APB1ENR_TIM3EN);
+	else if(Timer == TIM4)
+		RCC->APB1ENR &= ~(RCC_APB1ENR_TIM4EN);
+}
+/**
+	* @brief  Active l'horloge et règle l'ARR et le PSC du timer visé
+  * @note   Fonction à lancer avant toute autre. Le timer n'est pas encore lancé (voir MyTimerStart)
+	* @param  TIM_TypeDef Timer : indique le timer à utiliser par le chronomètre, TIM1, TIM2, TIM3 ou TIM4
+	* 				int Arr   : valeur à placer dans ARR
+	*					int Psc   : valeur à placer dans PSC
+  * @retval None
+  */
+
+void MyTimer_Conf(TIM_TypeDef * Timer,int Arr, int Psc) {
+	//active l'horloge pour les timers 2,3,4,5,6,7,12,13,14
+	if (Timer == TIM1 ) {
+		// On active l'horloge du timer 1
+		RCC->APB2ENR |= RCC_APB2ENR_IOPAEN ;
+	}
+	else if (Timer == TIM2 ) {
+		// On active l'horloge des timers 2
+		RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+	}
+	else if (Timer == TIM3) {
+		// On active l'horloge des timers 3
+		RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
+	}
+	else if (Timer == TIM4) {
+		// On active l'horloge des timers 4
+		RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
+	}
+	Timer->CR1 &= ~TIM_CR1_OPM;
+	// On met la valeur d'auto-reload dans le registre correspondant
+	Timer->ARR = Arr;
+	// Active la sortie du prescaler CK_CNT
+	Timer->CR1 |= TIM_CR1_CEN;
+	Timer->PSC = Psc;
+}
+void MyTimer_IT_Enable(TIM_TypeDef * Timer) {
+	if(Timer == TIM1) {
+		TIM1->DIER |= TIM_DIER_UIE;
+	}
+	else if (Timer == TIM2) {
+		TIM2->DIER |= TIM_DIER_UIE;
+	}
+	else if (Timer == TIM3) {
+		TIM3->DIER |= TIM_DIER_UIE;
+	}
+	else if (Timer == TIM4) {
+		TIM4->DIER |= TIM_DIER_UIE;
+	}
+	
+}
+void MyTimer_IT_Conf(TIM_TypeDef * Timer, void (*handler) (void),int priorite) {
+	if(Timer == TIM1) {
+		TIM1->DIER |= TIM_DIER_CC1IE;
+		NVIC->ISER[0] |= 1 << 27;
+		NVIC->IP[27] = priorite;
+		TIM1_handler_fct = handler;
+	}
+	else if (Timer == TIM2) {
+		TIM2->DIER |= TIM_DIER_CC1IE;
+		NVIC->ISER[0] |= 1 << 28;
+		NVIC->IP[28] = priorite;
+		TIM2_handler_fct = handler;
+	}
+	else if (Timer == TIM3) {
+		TIM3->DIER |= TIM_DIER_CC1IE;
+		NVIC->ISER[0] |= 1 << 29;
+		NVIC->IP[29] = priorite;
+		TIM3_handler_fct = handler;
+	}
+	else if (Timer == TIM4) {
+		TIM4->DIER |= TIM_DIER_CC1IE;
+		NVIC->ISER[0] |= 1 << 30;
+		NVIC->IP[30] = priorite;
+		TIM4_handler_fct = handler;
+	}
+}
+
+void TIM1_UP_IRQHandler(void) {
+	(*TIM1_handler_fct)();
+	//page 345
+	TIM1->SR &= ~TIM_SR_UIF;
+	
+}
+void TIM2_IRQHandler(void) {
+	(*TIM2_handler_fct)();
+	TIM2->SR &= ~TIM_SR_UIF;
+}
+void TIM3_IRQHandler(void) {
+	(*TIM3_handler_fct)();
+	TIM3->SR &= ~TIM_SR_UIF;
+}
+void TIM4_IRQHandler(void) {
+	(*TIM4_handler_fct)();
+	TIM4->SR &= ~TIM_SR_UIF;
+}
