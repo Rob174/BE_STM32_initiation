3d731f2502ea1895fc417efa0f1c742f6825e476
Rob174 <robin.128.456@gmail.com>
2020-11-06 12:49:39 +0100

    Commit initial Voilier ecoute

--- BE_STM32_voilier/Act3/ProjKEIL_Chrono_TIM_IT_LL/MDK-ARM/ecoute.c
+++ BE_STM32_voilier/Act3/ProjKEIL_Chrono_TIM_IT_LL/MDK-ARM/ecoute.c
@@ -10,13 +10,19 @@
  
 */ 
 #include "stm32f103xb.h" 
+#include "stm32f1xx_ll_tim.h"
 
 void (*TIM1_handler_fct)(void);
 void (*TIM2_handler_fct)(void);
 void (*TIM3_handler_fct)(void);
 void (*TIM4_handler_fct)(void);
 
-void MyTimer_Start(TIM_TypeDef * Timer)
+/* Philosophie globale 
+cf p 366 et 384
+On a deux voies pour un décompte de la période globale et un décompte du temps au niveau haut
+
+*/
+void MyEcoute_Start(TIM_TypeDef * Timer)
 {
 	/*
 	Timer = TIM1, TIM2, TIM3 ou TIM4:
@@ -38,7 +44,7 @@ void MyTimer_Start(TIM_TypeDef * Timer)
 		RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
 }
 
-void MyTimer_Stop(TIM_TypeDef * Timer)
+void MyEcoute_Stop(TIM_TypeDef * Timer)
 {
 	/*
 	Timer = TIM1, TIM2, TIM3 ou TIM4:
@@ -68,7 +74,7 @@ void MyTimer_Stop(TIM_TypeDef * Timer)
   * @retval None
   */
 
-void MyTimer_Conf(TIM_TypeDef * Timer,int Arr, int Psc) {
+void MyEcoute_Conf(TIM_TypeDef * Timer, void (*handler) (void), int priorite) {
 	//active l'horloge pour les timers 2,3,4,5,6,7,12,13,14
 	if (Timer == TIM1 ) {
 		// On active l'horloge du timer 1
@@ -87,13 +93,61 @@ void MyTimer_Conf(TIM_TypeDef * Timer,int Arr, int Psc) {
 		RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
 	}
 	Timer->CR1 &= ~TIM_CR1_OPM;
-	// On met la valeur d'auto-reload dans le registre correspondant
-	Timer->ARR = Arr;
+	//Reprise de l'exemple de la page 384
+	//Sélection paramètres pour TI1FP1 et TI1FP2 (1 ou 2 noté par a)
+	//Configuration de TI1FP1
+	//Sélectionner input CC1S de TIMx_CCMR1 (p414)
+	Timer->CCMR1 |= TIM_CCMR1_CC1S_0;
+	//Choix de la polarité (high = front montant) CC1P de TIMx_CCER (p417)
+	Timer->CCER |= ~TIM_CCER_CC1P; //active high
+	//Configuration de TI1FP2
+	//Sélectionner input CC1S de TIMx_CCMR1 (p414)
+	Timer->CCMR2 |= TIM_CCMR1_CC1S_1;
+	//Choix de la polarité (high = front montant) CC1P de TIMx_CCER (p417)
+	Timer->CCER |= TIM_CCER_CC1P;//active low
+	//Configuration TI1FP1 et TI1FP2
+	//On attend 8/72MHz ~ 0.1µs avant de comptabiliser une transition : choix du filtre (p455)
+	Timer->CCMR1 |= TIM_CCMR1_IC1F_0 | TIM_CCMR1_IC1F_1;
+	
+	//Choisir le déclencheur TS  de TIMx_SMCR (p406 ici 407)
+	Timer->SMCR |= (TIM_SMCR_TS_0 | TIM_SMCR_TS_2);
+	//Configurer le contrôleur en mode reset (recompte depuis 0 à chaque front montant) SMS de TIMx_SMCR
+	Timer->SMCR |= TIM_SMCR_SMS_2;
+	//Choix du prescaler pour le comptage
+	//Servo : Niveau haut entre 1 et 2ms et période de 20ms
+	//Val max de compteur 2^16 ; en mettant 1 prescaler de 100 on aura 2ms=1440 comptages et 20ms=14400 comptages < 65536
+	Timer->PSC = 100;//TOASK : --> à mettre en couche service ???
+	//Ne touche pas à l'ARR car le compteur sera reset à chaque front montant d'après qlqs lignes au dessus
+	//Activer la capture CC1E et CC2E de TIMx_CCER
+	Timer->CCER |= TIM_CCER_CC1E | TIM_CCER_CC2E;
+	
 	// Active la sortie du prescaler CK_CNT
 	Timer->CR1 |= TIM_CR1_CEN;
-	Timer->PSC = Psc;
+	// Préparation récupération valeur
+	Timer->DIER |= LL_TIM_DIER_CC1IE; //active l'interruption 1
+
+	if (Timer == TIM1 ) {
+		NVIC->ISER[0] |= 1 << 27;
+		NVIC->IP[27] = priorite;
+		TIM1_handler_fct = handler;
+	}
+	else if (Timer == TIM2 ) {
+		NVIC->ISER[0] |= 1 << 28;
+		NVIC->IP[28] = priorite;
+		TIM2_handler_fct = handler;
+	}
+	else if (Timer == TIM3) {
+		NVIC->ISER[0] |= 1 << 29;
+		NVIC->IP[29] = priorite;
+		TIM3_handler_fct = handler;
+	}
+	else if (Timer == TIM4) {
+		NVIC->ISER[0] |= 1 << 30;
+		NVIC->IP[30] = priorite;
+		TIM4_handler_fct = handler;
+	}
 }
-void MyTimer_IT_Enable(TIM_TypeDef * Timer) {
+void MyEcoute_IT_Enable(TIM_TypeDef * Timer) {
 	if(Timer == TIM1) {
 		TIM1->DIER |= TIM_DIER_UIE;
 	}
@@ -106,50 +160,23 @@ void MyTimer_IT_Enable(TIM_TypeDef * Timer) {
 	else if (Timer == TIM4) {
 		TIM4->DIER |= TIM_DIER_UIE;
 	}
-	
-}
-void MyTimer_IT_Conf(TIM_TypeDef * Timer, void (*handler) (void),int priorite) {
-	if(Timer == TIM1) {
-		TIM1->DIER |= TIM_DIER_CC1IE;
-		NVIC->ISER[0] |= 1 << 27;
-		NVIC->IP[27] = priorite;
-		TIM1_handler_fct = handler;
-	}
-	else if (Timer == TIM2) {
-		TIM2->DIER |= TIM_DIER_CC1IE;
-		NVIC->ISER[0] |= 1 << 28;
-		NVIC->IP[28] = priorite;
-		TIM2_handler_fct = handler;
-	}
-	else if (Timer == TIM3) {
-		TIM3->DIER |= TIM_DIER_CC1IE;
-		NVIC->ISER[0] |= 1 << 29;
-		NVIC->IP[29] = priorite;
-		TIM3_handler_fct = handler;
-	}
-	else if (Timer == TIM4) {
-		TIM4->DIER |= TIM_DIER_CC1IE;
-		NVIC->ISER[0] |= 1 << 30;
-		NVIC->IP[30] = priorite;
-		TIM4_handler_fct = handler;
-	}
 }
 
-void TIM1_UP_IRQHandler(void) {
+void TIM1_UP_Ecoute_IRQHandler(void) {
 	(*TIM1_handler_fct)();
 	//page 345
 	TIM1->SR &= ~TIM_SR_UIF;
 	
 }
-void TIM2_IRQHandler(void) {
+void TIM2_Ecoute_IRQHandler(void) {
 	(*TIM2_handler_fct)();
 	TIM2->SR &= ~TIM_SR_UIF;
 }
-void TIM3_IRQHandler(void) {
+void TIM3_Ecoute_IRQHandler(void) {
 	(*TIM3_handler_fct)();
 	TIM3->SR &= ~TIM_SR_UIF;
 }
-void TIM4_IRQHandler(void) {
+void TIM4_Ecoute_IRQHandler(void) {
 	(*TIM4_handler_fct)();
 	TIM4->SR &= ~TIM_SR_UIF;
-		}
+}
---
+++ BE_STM32_voilier/Act3/ProjKEIL_Chrono_TIM_IT_LL/Services/Chrono.c
@@ -0,0 +1,159 @@
+// A COMPLETER
+
+/*
+Service permettant de chornométrer jusqu'à 59mn 59s 99 1/100
+Utilise un timer au choix (TIMER1 à TIMER4).
+Utilise la lib MyTimers.h /.c
+*/
+
+
+
+#include "Chrono.h"
+#include "MyTimer.h"
+#include "stm32f1xx_ll_gpio.h"
+#define ETAT_STOP 0
+#define ETAT_START 1
+#define ETAT_RESET 2
+
+// variable privée de type Time qui mémorise la durée mesurée
+static Time Chrono_Time; // rem : static rend la visibilité de la variable Chrono_Time limitée à ce fichier 
+
+// variable privée qui mémorise pour le module le timer utilisé par le module
+static TIM_TypeDef * Chrono_Timer=TIM1; // init par défaut au cas où l'utilisateur ne lance pas Chrono_Conf avant toute autre fct.
+static int etat = ETAT_RESET;
+
+/**
+	* @brief  incrémente la variable privée Chron_Time modulo 60mn 
+  * @note   
+	* @param  Aucun
+  * @retval Aucun
+  */
+void Chrono_Task_10ms(void)
+{ 
+	Chrono_Time.Hund++;
+	if (Chrono_Time.Hund==100)
+	{
+		Chrono_Time.Sec++;
+		Chrono_Time.Hund=0;
+		if (etat == ETAT_START)
+			LL_GPIO_TogglePin(GPIOC,LL_GPIO_PIN_10);
+	}
+	if (Chrono_Time.Sec==60)
+	{
+		Chrono_Time.Min++;
+		Chrono_Time.Sec=0;
+	}
+	if (Chrono_Time.Min==60)
+	{
+		Chrono_Time.Hund=0;
+	}
+}
+void Chrono_Conf_io(void) {
+	//bouton USER : START / STOP
+	LL_GPIO_SetPinMode(GPIOC,LL_GPIO_PIN_13,LL_GPIO_MODE_FLOATING);
+	//bouton Exterieur : RESET
+	LL_GPIO_SetPinMode(GPIOC,LL_GPIO_PIN_8,LL_GPIO_MODE_FLOATING);
+	// LED
+	LL_GPIO_SetPinMode(GPIOC,LL_GPIO_PIN_10,LL_GPIO_MODE_OUTPUT_10MHz);
+	LL_GPIO_SetPinOutputType(GPIOC,LL_GPIO_PIN_10,LL_GPIO_OUTPUT_PUSHPULL);
+}
+void Chrono_Background(void) {
+	if(LL_GPIO_IsInputPinSet(GPIOC,LL_GPIO_PIN_13)) {
+		if (etat == ETAT_RESET) {
+			etat = ETAT_START;
+			Chrono_Start();
+		}
+		else if (etat == ETAT_START) {
+			etat = ETAT_STOP;
+			Chrono_Stop();
+		}
+	}
+	if(LL_GPIO_IsInputPinSet(GPIOC,LL_GPIO_PIN_8)) {
+		etat = ETAT_RESET;
+		Chrono_Reset();
+	}
+}
+
+/**
+	* @brief  Configure le chronomètre. 
+  * @note   A lancer avant toute autre fonction.
+	* @param  Timer : indique le timer à utiliser par le chronomètre, TIM1, TIM2, TIM3 ou TIM4
+  * @retval None
+  */
+void Chrono_Conf(TIM_TypeDef * Timer)
+{
+	// Reset Time
+	Chrono_Time.Hund=0;
+	Chrono_Time.Sec=0;
+	Chrono_Time.Min=0;
+	
+	// Fixation du Timer
+	Chrono_Timer=Timer;
+
+	// Réglage Timer pour un débordement à 10ms
+	MyTimer_Conf(Chrono_Timer,999, 719);
+	
+	// Réglage interruption du Timer avec callback : Chrono_Task_10ms()
+	MyTimer_IT_Conf(Chrono_Timer, Chrono_Task_10ms,3);
+	
+	// Validation IT
+	MyTimer_IT_Enable(Chrono_Timer);
+	
+	//Configuration des 3 ios (2BP + LED)
+	Chrono_Conf_io();
+}
+
+
+/**
+	* @brief  Démarre le chronomètre. 
+  * @note   si la durée dépasse 59mn 59sec 99 Hund, elle est remise à zéro et repart
+	* @param  Aucun
+  * @retval Aucun
+  */
+void Chrono_Start(void)
+{
+	MyTimer_Start(Chrono_Timer);
+}
+
+
+/**
+	* @brief  Arrête le chronomètre. 
+  * @note   
+	* @param  Aucun
+  * @retval Aucun
+  */
+void Chrono_Stop(void)
+{
+	MyTimer_Stop(Chrono_Timer);
+}
+
+
+/**
+	* @brief  Remet le chronomètre à 0 
+  * @note   
+	* @param  Aucun
+  * @retval Aucun
+  */
+void Chrono_Reset(void)
+{
+  // Arrêt Chrono
+	MyTimer_Stop(Chrono_Timer);
+
+	// Reset Time
+	Chrono_Time.Hund=0;
+	Chrono_Time.Sec=0;
+	Chrono_Time.Min=0;
+}
+
+
+/**
+	* @brief  Renvoie l'adresse de la variable Time privée gérée dans le module Chrono.c
+  * @note   
+	* @param  Aucun
+  * @retval adresse de la variable Time
+  */
+Time * Chrono_Read(void)
+{
+	return &Chrono_Time;
+}
+
